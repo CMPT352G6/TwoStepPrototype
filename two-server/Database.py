35e663304b3d2804c8c1fd63f28b9c629887a1ee
DSakura1987 <taoda001@gmail.com>
2014-03-23 11:51:36 -0600

    Change user list structure

--- two-server/Database.py
+++ two-server/Database.py
@@ -2,7 +2,8 @@ from google.appengine.ext import ndb
 
 class UserList(ndb.Model):
     """Models an individual Guestbook entry with author, content, and date."""
-    userName = ndb.StringProperty()
-    userAlgorithm = ndb.StringProperty(indexed=False)
-    userSecretKey = ndb.StringProperty(indexed=False)
-
+    Username = ndb.StringProperty(required = True)
+    Password = ndb.StringProperty(required = True)
+    SecretKey = ndb.StringProperty(required = True, indexed=False)
+    Counter = ndb.StringProperty()
+    CounterTime = ndb.DateTimeProperty(auto_now_add=True)
