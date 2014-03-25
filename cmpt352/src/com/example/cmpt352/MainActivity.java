6b5d511661f1ff6a78caf4ce297ff85e008fef8f
guoxingze <guoxingze@gmail.com>
2014-03-25 01:47:29 -0600

    finished android part, except password algorithm

--- cmpt352/src/com/example/cmpt352/MainActivity.java
+++ cmpt352/src/com/example/cmpt352/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.cmpt352;
 
 import android.os.Bundle;
+import android.provider.Settings.Secure;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -11,8 +15,10 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity {
 	private Button generateB;
+	private Button deviceID;
 	private TextView password;
 	private EditText code;
+	private String android_id;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +28,8 @@ public class MainActivity extends Activity {
 		generateB = (Button)findViewById(R.id.button1);
 		password = (TextView)findViewById(R.id.TextView01);
 		code = (EditText)findViewById(R.id.editText);
+		android_id = Secure.getString(getBaseContext().getContentResolver(),Secure.ANDROID_ID); 
+		deviceID = (Button)findViewById(R.id.idButton);
 		
 		generateB.setOnClickListener(new OnClickListener(){
 
@@ -30,9 +38,14 @@ public class MainActivity extends Activity {
 				int newCode = Integer.parseInt(code.getText().toString());
 				int newPassword = getPassword(newCode);
 				password.setText(String.valueOf(newPassword));
+				code.setText("");
+				System.out.println("~~~~~~~~~~~"+android_id);
 			}
 			
 		});
+		
+		setDeviceIDListener();
+		
 	}
 
 	private int getPassword(int inCode){
@@ -42,5 +55,19 @@ public class MainActivity extends Activity {
 		newPassword = inCode*12/15;
 		
 		return newPassword;
+	}
+	
+	private void setDeviceIDListener(){
+		deviceID.setOnClickListener(new OnClickListener(){
+
+			@Override
+			public void onClick(View v) {
+				AlertDialog builder = new AlertDialog.Builder(MainActivity.this)
+                .setTitle("Device ID")
+                .setMessage(android_id)
+                .setPositiveButton("Confirm", null)
+                .show(); 
+			}
+		});
 	}
 }
