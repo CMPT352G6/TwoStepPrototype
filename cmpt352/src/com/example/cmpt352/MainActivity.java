55437b144689611a00aadd3e3811270bf2a72664
guoxingze <guoxingze@gmail.com>
2014-03-25 21:41:11 -0600

    completed anrdoird

--- cmpt352/src/com/example/cmpt352/MainActivity.java
+++ cmpt352/src/com/example/cmpt352/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.cmpt352;
 
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
 import android.os.Bundle;
 import android.provider.Settings.Secure;
 import android.app.Activity;
@@ -11,22 +14,26 @@ import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.HorizontalScrollView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	private Button generateB;
 	private Button deviceID;
-	private TextView password;
+	private EditText password;
 	private EditText code;
 	private String android_id;
+	private hotp HOTPalgorithm;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		HOTPalgorithm = new hotp();
 		setContentView(R.layout.activity_main);
 		
 		generateB = (Button)findViewById(R.id.button1);
-		password = (TextView)findViewById(R.id.TextView01);
+		password = (EditText)findViewById(R.id.TextView01);
 		code = (EditText)findViewById(R.id.editText);
 		android_id = Secure.getString(getBaseContext().getContentResolver(),Secure.ANDROID_ID); 
 		deviceID = (Button)findViewById(R.id.idButton);
@@ -35,11 +42,17 @@ public class MainActivity extends Activity {
 
 			@Override
 			public void onClick(View v) {
-				int newCode = Integer.parseInt(code.getText().toString());
-				int newPassword = getPassword(newCode);
-				password.setText(String.valueOf(newPassword));
-				code.setText("");
-				System.out.println("~~~~~~~~~~~"+android_id);
+				try{
+//				if(code.getText().toString().length()>0){
+					int newCode = Integer.parseInt(code.getText().toString());
+					String newPassword = getPassword(newCode);
+					password.setText(String.valueOf(newPassword));
+					code.setText("");
+				}
+				catch(NumberFormatException e){
+					 Toast.makeText(getApplicationContext(), "Please input valid code", Toast.LENGTH_SHORT).show(); 
+				}
+				
 			}
 			
 		});
@@ -48,11 +61,17 @@ public class MainActivity extends Activity {
 		
 	}
 
-	private int getPassword(int inCode){
-		int newPassword;
+	private String getPassword(int inCode){
+		byte[] deviceCode = android_id.getBytes();
+		String newPassword="";
+		try {
+		newPassword = HOTPalgorithm.generateOTP(deviceCode, inCode, 8, false, inCode);
+		}
+	    catch (Exception e) {
+	        System.out.println("Exception = " + e);
+	        }
 		//to do 
 		//edit algorithm
-		newPassword = inCode*12/15;
 		
 		return newPassword;
 	}
