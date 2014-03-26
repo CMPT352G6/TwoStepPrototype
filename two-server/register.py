fe17546422ce84768eae884ac8ba193e44cf7696
DSakura1987 <taoda001@gmail.com>
2014-03-26 09:29:17 -0600

    Add comments for files
    Also removed unneed files

--- two-server/register.py
+++ two-server/register.py
@@ -3,6 +3,7 @@ from Database import UserList
 
 class RegisterHandler(webapp2.RequestHandler):
 
+    # Deny GET request.
     def get(self):
         self.denyIllegalAccess('Method Not Allowed')
     def post(self):
@@ -10,6 +11,7 @@ class RegisterHandler(webapp2.RequestHandler):
         username = self.request.get("username","")
         password = self.request.get("password","")
         secretKey = self.request.get("serialNumber","")
+        # Add the user's details when no user existed with the same user name.
         if not self.isUserExisted(username):
             user = UserList()
             user.Username = username
@@ -18,6 +20,7 @@ class RegisterHandler(webapp2.RequestHandler):
             user.put()
             self.setSuccessResponse()
 
+        # Deny adding when a duplicated user is found.
         else:
             self.denyIllegalAccess('Username Existed')
     def isUserExisted(self, username):
@@ -26,11 +29,13 @@ class RegisterHandler(webapp2.RequestHandler):
         # Expected to get None for True
         return UserList.query(UserList.Username == username).get() != None
 
+    # Send success message.
     def setSuccessResponse(self):
         self.response.status = 200
         self.response.body = json.dumps({"success": True})
         self.response.content_type = 'application/json'
 
+    # Send failure message.
     def denyIllegalAccess(self, reason):
         self.response.status = 403
         self.response.body = json.dumps({"success": False, "reason": reason})
